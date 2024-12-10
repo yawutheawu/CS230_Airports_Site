3d18435f36e10a0cfb303a7b38659ac82e7dd1a3
yawutheawu <mayklprime@gmail.com>
2024-12-10 17:18:35 -0500

    deleted broken code

--- updateFiles.py
+++ updateFiles.py
@@ -15,7 +15,7 @@ import streamlit as st
 
 import functions as funcs
 
-@st.experimental_singleton
+
 def installff():
   os.system('sbase install geckodriver')
   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
